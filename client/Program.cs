8090144f6c92a03d2501db8ccf97884cb2848cd6
Daniel Szwed <dszwed@prescientco.com>
2023-01-12 21:05:24 +0100

    add client

---
+++ client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace RestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+                Console.WriteLine("Podaj liczby oddzielone srednikami:");
+
+                var input = Console.ReadLine();
+
+                var numbers = Array.ConvertAll(input.Trim().Split(';', options: StringSplitOptions.RemoveEmptyEntries), Double.Parse);
+    
+                var sorted = string.Join("; ", Sort(numbers).Select(numbers => numbers.ToString()).ToArray());
+
+                Console.WriteLine($"Wynik: {sorted}");
+            }
+        }
+
+        static double[] Sort(double[] numbers) {
+            using var client = new HttpClient();
+            var serialized = JsonSerializer.Serialize(numbers);
+            var conent = new StringContent(serialized, Encoding.UTF8, "application/json");
+            var response = client.PostAsync("http://localhost:8080/sort", conent).GetAwaiter().GetResult();
+            var responseString = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            
+            return JsonSerializer.Deserialize<double[]>(responseString);
+        }
+    }
+}
